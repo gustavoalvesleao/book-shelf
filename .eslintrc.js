dab00d71c3387c9f8ed010d9247cb9f7e8c777eb
Gustavo <gustavoalvesleao@gmail.com>
2021-10-25 15:24:53 -0300

    add eslintrc file

---
+++ .eslintrc.js
@@ -0,0 +1,72 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    "react-app",
+    "airbnb",
+    "prettier",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  plugins: ["react", "import", "jsx-a11y"],
+  rules: {
+    "react/jsx-filename-extension": [
+      "error",
+      {
+        extensions: [".tsx"],
+      },
+    ],
+    "react/jsx-one-expression-per-line": "off",
+    "import/extensions": "off",
+    "import/prefer-default-export": "off",
+    "react/require-default-props": "off",
+    "@typescript-eslint/explicit-member-accessibility": "off",
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"],
+    "react/jsx-props-no-spreading": 0,
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          ["builtin", "external"],
+          "internal",
+          "parent",
+          ["sibling", "index"],
+        ],
+        "newlines-between": "always-and-inside-groups",
+      },
+    ],
+  },
+  overrides: [
+    {
+      // enable the rule specifically for TypeScript files
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "@typescript-eslint/explicit-function-return-type": ["warn"],
+      },
+    },
+  ],
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {},
+    },
+  },
+};
