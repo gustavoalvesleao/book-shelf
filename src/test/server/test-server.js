b94f546c62e17840bf044d87569b39c5f62a3b52
Gustavo <gustavoalvesleao@gmail.com>
2021-10-29 11:37:38 -0300

    feat: add mockup msw server

---
+++ src/test/server/test-server.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+import { setupServer } from "msw/node";
+import { handlers } from "./server-handlers";
+
+const server = setupServer(...handlers);
+
+export * from "msw";
+export { server };
