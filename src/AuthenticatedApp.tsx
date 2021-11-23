48e2ae9c532c0c18b2c43ee1c0d2ea3416b405bf
Gustavo <gustavoalvesleao@gmail.com>
2021-11-23 11:08:49 -0300

    feat: add styles for matching route

--- src/AuthenticatedApp.tsx
+++ src/AuthenticatedApp.tsx
@@ -3,7 +3,7 @@
 // eslint-disable-next-line
 import { jsx } from "@emotion/react";
 import React from "react";
-import { Routes, Route, Link, LinkProps } from "react-router-dom";
+import { Routes, Route, Link, LinkProps, useMatch } from "react-router-dom";
 
 import DiscoverBooksScreen from "screens/DiscoverBooks";
 import NotFoundScreen from "screens/NotFound";
@@ -70,23 +70,35 @@ function AuthenticatedApp({ user, logout }: Props) {
 }
 
 function NavLink(props: Omit<LinkProps, "className" | "style">) {
+  const { to } = props;
+  const match = useMatch(to.toString());
+
   return (
     <Link
-      css={{
-        display: "block",
-        padding: "8px 15px 8px 10px",
-        margin: "5px 0",
-        width: "100%",
-        height: "100%",
-        color: colors.text,
-        borderRadius: "2px",
-        borderLeft: "5px solid transparent",
-        ":hover": {
-          color: colors.indigo,
-          textDecoration: "none",
-          background: colors.gray10,
+      css={[
+        {
+          display: "block",
+          padding: "8px 15px 8px 10px",
+          margin: "5px 0",
+          width: "100%",
+          height: "100%",
+          color: colors.text,
+          borderRadius: "2px",
+          borderLeft: "5px solid transparent",
+          ":hover": {
+            color: colors.indigo,
+            textDecoration: "none",
+            background: colors.gray10,
+          },
         },
-      }}
+        match
+          ? {
+              borderLeft: `5px solid ${colors.indigo}`,
+              background: colors.gray10,
+              ":hover": { background: colors.gray20 },
+            }
+          : null,
+      ]}
       {...props}
     />
   );
